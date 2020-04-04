14b4c83791dcb95f7e43bf5dc70268365833d6fe
Lluke-h <45208869+Lluke-h@users.noreply.github.com>
2020-04-04 19:21:58 +0200

    initial commit with basic functionalities : shared drawing canvas and chat room

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+let players = {};
+
+
+//on connect
+//Whenever someone connects this gets executed
+
+io.on('connection', function (socket) {
+
+    // New connection handling
+    socket.on('new-user', name => {
+        // io.sockets.emit('playerupdate', players);
+
+        players[socket.id] = {id: socket.id, name: name, score: 0, drawing: false};
+        console.log(`player ${name} has connected`);
+        socket.broadcast.emit('user-connected', name);
+
+        if (players !== {}){
+            console.log('requested drawing from all ');
+            socket.broadcast.emit('send-drawing', 'please send drawing');
+            socket.on('whole-drawing', drawing => {
+                console.log('sending drawing to', players[socket.id].name);
+                socket.broadcast.emit('receive-drawing', drawing)
+            })
+
+
+        }
+    });
+
+
+    // Chat messages
+    socket.on('send-chat-message', message => {
+        console.log(message);
+        socket.broadcast.emit('chat-message', {message: message, name: players[socket.id].name});
+    });
+    // Drawing
+
+    socket.on('draw', (data) => {
+        socket.broadcast.emit('draw', data);
+    });
+    socket.on('startdraw', (data) => {
+        socket.broadcast.emit('startdraw');
+    });
+    socket.on('stopdraw', (data) => {
+        socket.broadcast.emit('stopdraw');
+    });
+
+
+    //Whenever someone disconnects
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        socket.broadcast.emit('user-disconnected', players[socket.id].name);
+        delete players[socket.id];
+        // io.sockets.emit('playerupdate', players);
+    });
+});
+
+server.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+
+// let counter = 0;
+// setInterval(function() {
+//     counter = counter + 1;
+//     io.sockets.emit('message', counter);
+// }, 1000);
---
+++ public/pictionnary.js
@@ -0,0 +1,147 @@
+// const socket = io();
+const socket = io.connect('http://localhost:3000');
+
+
+//
+
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+const messageContainer = document.getElementById('message-container');
+
+// ---------------- Chat ------------------
+//sending a message
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    console.log(message);
+    appendMessage(`You : ${message}`);
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+});
+//Receiving a message
+socket.on('chat-message', data => appendMessage(`${data.name} : ${data.message}`));
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} has joined the game`)
+});
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} has left the game`)
+});
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.appendChild(messageElement)
+}
+
+// -------------- Join game ---------------
+
+const name = prompt('Enter your name');
+appendMessage('You joined the game');
+socket.emit('new-user', name);
+socket.on('receive-drawing', drawing => {
+    console.log('received drawing');
+    const img = new Image();
+    img.onload = () => ctx.drawImage(img, 0, 0);
+    img.src = drawing;
+});
+
+
+// ------------------------ Drawing -------------------------
+
+
+socket.on('message', socket => console.log(socket));
+socket.on('draw', data => {
+    isDrawing = true;
+    console.log('received coords');
+    draw(data);
+
+    isDrawing = false
+});
+socket.on('startdraw', startDraw);
+socket.on('stopdraw', stopDraw);
+socket.on('playerupdate', players => updatePlayercards(players));
+
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+ctx.strokeStyle = '#BADA55';
+
+let isDrawing = false;
+let lastX = -1;
+let lastY = -1;
+let hue = 0;
+linewidth = 0;
+
+function draw(e) {
+    if (!isDrawing) return;
+    // ctx.strokeStyle = `hsl(${hue}, 86%, 50%)`;
+    ctx.strokeStyle = `1, 86%, 50%)`;
+    ctx.lineWidth = 10; //+ 15 * Math.sin(linewidth);
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    hue += 5;
+    linewidth += 0.1;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+
+}
+
+function startDraw() {
+    isDrawing = true;
+    [lastX, lastY] = [this.offsetX, this.offsetY];
+}
+
+function stopDraw() {
+    isDrawing = false
+}
+
+function updatePlayercards(players) {
+    console.log('update player cards');
+    const playercards = document.querySelector('.playercards');
+    playercards.innerHTML = "";
+
+    // players.forEach(player => {
+    //     console.log('added playerCard !!!');
+    //     const card = document.createElement('div');
+    //     card.textContent = player.id;
+    //     card.setAttribute('class', 'playercard');
+    //     playercards.appendChild(card);
+    // });
+}
+
+canvas.addEventListener('mousemove', (e) => {
+    draw(e);
+    if (isDrawing) {
+        const {offsetX, offsetY} = e;
+        socket.emit('draw', {offsetX: offsetX, offsetY: offsetY});
+    }
+});
+
+canvas.addEventListener('mousedown', () => {
+    startDraw();
+    socket.emit('startdraw')
+});
+
+canvas.addEventListener('mouseup', () => {
+    stopDraw();
+    socket.emit('stopdraw')
+});
+
+canvas.addEventListener('mouseout', () => {
+    stopDraw();
+    socket.emit('stopdraw')
+});
+
+//send whole drawing if asked
+
+socket.on('send-drawing', () => {
+    drawing = canvas.toDataURL();
+    socket.emit('whole-drawing', drawing);
+    console.log('Sending back drawing');
+});
+
